513cdfa341794dacdb0d278f6413bebe295313fa
Samer Masry <samer.masry@gmail.com>
2017-12-14 16:22:17 -0800

    Changing config

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
-# name: discourse-date-filter
-# about: Allowing a date filter
+# name: discourse-period-filter
+# about: Allowing a period filter
 # version: 0.1
 # authors: Samer Masry
-# url: https://github.com/smasry/discourse-date-filter
+# url: https://github.com/smasry/discourse-period-filter
 
-enabled_site_setting :date_filter_enabled
+enabled_site_setting :period_filter_enabled
